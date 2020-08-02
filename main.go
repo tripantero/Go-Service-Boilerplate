fc68a616dfdf0629b06043c290df0a396adcbd5a
Entalizen <danielarya185@gmail.com>
2020-08-03 00:38:37 +0900

    feat(./): run server when application is started

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/Satssuki/Go-Service-Boilerplate/helpers"
+	"github.com/Satssuki/Go-Service-Boilerplate/route"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
+	route.SetupRouter().Run(helpers.GetPORT())
 }

a36e2702f277813509282d9f443d76faa0863fd7
Entalizen <danielarya185@gmail.com>
2020-08-03 00:31:59 +0900

    fix(helpers/api): switch JSONReponse data parameter type to empty interface

--- helpers/api/response.go
+++ helpers/api/response.go
@@ -4,12 +4,11 @@ import (
 	"net/http"
 
 	"github.com/Satssuki/Go-Service-Boilerplate/helpers"
-	"github.com/gin-gonic/gin"
 )
 
 // JSONResponse function supposed to be transform fetched struct to bytes and respond it
-func JSONResponse(statusCode int, writer http.ResponseWriter, data *gin.H) error {
+func JSONResponse(statusCode int, writer http.ResponseWriter, data interface{}) error {
 	writer.WriteHeader(statusCode)
 	writer.Header().Add("Content-Type", "application/json")
-	return helpers.JSONNewEncoder(writer).Encode(*data)
+	return helpers.JSONNewEncoder(writer).Encode(data)
 }

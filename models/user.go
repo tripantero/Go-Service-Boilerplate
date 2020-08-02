3b6c5c5aaeea392d232ff84550ab110a233200a9
Entalizen <danielarya185@gmail.com>
2020-08-03 05:42:10 +0900

    refactor(models): move action to database in service folders

--- models/user.go
+++ models/user.go
@@ -2,12 +2,6 @@ package models
 
 import (
 	"github.com/Kamva/mgm/v3"
-	"go.mongodb.org/mongo-driver/bson"
-)
-
-var (
-	// collStruct yep again this is just dummy objet for initializing the collection naame
-	collStruct = &User{}
 )
 
 // User just dummy struct
@@ -15,18 +9,4 @@ type User struct {
 	mgm.DefaultModel `bson:",inline"`
 	Name             string `bson:"name" json:"name"`
 	Age              int    `bson:"age"  json: "age"`
-}
-
-// InsertUser single function to insert User
-func InsertUser(name string, age int) error {
-	user := &User{Name: name, Age: age}
-	return Create(user)
-}
-
-// FindAllUsers return user list from database
-func FindAllUsers() (*[]User, error) {
-	users := []User{}
-	cursor, err := mgm.Coll(collStruct).Find(mgm.Ctx(), bson.M{})
-	cursor.All(mgm.Ctx(), &users)
-	return &users, err
 }

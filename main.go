fd0d8985faba3bf2e6d22916adcfcb2c3030513f
Entalizen <danielarya185@gmail.com>
2020-08-03 04:18:41 +0900

    feat(./): load env, connect to database, run http in main.go

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/Satssuki/Go-Service-Boilerplate/helpers"
+	"github.com/Satssuki/Go-Service-Boilerplate/models"
 	"github.com/Satssuki/Go-Service-Boilerplate/route"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
-	route.SetupRouter().Run(helpers.GetPORT())
+	err := godotenv.Load()
+	helpers.PanicIfErrNotNil(err)
+
+	err = models.Connect()
+	helpers.PanicIfErrNotNil(err)
+
+	err = route.SetupRouter().Run(helpers.GetPORT())
+	helpers.PanicIfErrNotNil(err)
 }

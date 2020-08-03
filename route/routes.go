44d221e72a9255f19017575ebacd73386334e91c
Entalizen <danielarya185@gmail.com>
2020-08-03 19:26:29 +0900

    feat(route): register insert user controller on route service

--- route/routes.go
+++ route/routes.go
@@ -1,11 +1,17 @@
 package route
 
 import (
+	v1 "github.com/Satssuki/Go-Service-Boilerplate/controllers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
 // SetupRouter Register each route here
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	v1g := router.Group("/api/v1")
+	{
+		v1g.POST("/user", v1.InsertUser)
+	}
 	return router
 }

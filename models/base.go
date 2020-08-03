a19de631355041079bd6df9ef325a127e7e848c8
Entalizen <danielarya185@gmail.com>
2020-08-03 18:40:50 +0900

    refactor(models): remove Create function in models/base.go

--- models/base.go
+++ models/base.go
@@ -11,10 +11,3 @@ import (
 func Connect() error {
 	return mgm.SetDefaultConfig(nil, os.Getenv("DBNAME"), options.Client().ApplyURI(os.Getenv("DBURI")))
 }
-
-// Create simplify insert document to collection
-// but please pass reference parameter with reference of object that already created
-func Create(reference interface{}) error {
-	convertedRef := reference.(mgm.Model)
-	return mgm.Coll(convertedRef).Create(convertedRef)
-}

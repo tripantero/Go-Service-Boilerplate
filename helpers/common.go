cfc8b41a2cebbc7273aa88b0aa3a648d2509117d
Entalizen <danielarya185@gmail.com>
2020-08-03 20:09:31 +0900

    fix(helpers): GetPORT function not doing well so i switched port from 4 length to 3 length

--- helpers/common.go
+++ helpers/common.go
@@ -5,7 +5,7 @@ import "os"
 // GetPORT function to validate the port selection
 func GetPORT() string {
 	port := os.Getenv("PORT")
-	if len(port) <= 4 {
+	if len(port) < 3 {
 		port = ":6007"
 	}
 	return port

a36348f7f09ccaaa714dd7aed581dfbf91560c1f
Entalizen <danielarya185@gmail.com>
2020-08-03 19:26:02 +0900

    feat(controllers/api/v1): add controller for sample insert user

--- controllers/api/v1/users.go
+++ controllers/api/v1/users.go
@@ -1,9 +1,35 @@
 package v1
 
 import (
+	"net/http"
+
+	"github.com/Satssuki/Go-Service-Boilerplate/helpers"
+	"github.com/Satssuki/Go-Service-Boilerplate/helpers/api"
+	v1s "github.com/Satssuki/Go-Service-Boilerplate/services/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
+// InsertUser sample controller to perform insert user function
 func InsertUser(c *gin.Context) {
-	// userService := v1s.UserService{}
+	defer c.Request.Body.Close()
+	service := v1s.CreateUserService()
+	bytes, err := helpers.ReadAllByteFromIO(c.Request.Body)
+	if err == nil {
+		err = helpers.ByteToStruct(bytes, &service.User)
+		if err == nil {
+			err = service.Insert()
+			if err == nil {
+				api.JSONResponse(http.StatusOK, c.Writer, gin.H{
+					"status":  "ok",
+					"message": "user defined",
+				})
+				return
+			}
+		}
+	}
+
+	api.JSONResponse(http.StatusBadRequest, c.Writer, gin.H{
+		"status":  "failure",
+		"message": err.Error(),
+	})
 }

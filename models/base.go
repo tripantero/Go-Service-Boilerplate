daa8f432be81212f058fe59b7ad1f6bbaeba4aed
Entalizen <danielarya185@gmail.com>
2020-08-03 18:43:46 +0900

    feat(models): create interface to declare common behaviour of model type

--- models/base.go
+++ models/base.go
@@ -7,6 +7,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// BaseModel declare the common action that model can do
+type BaseModel interface {
+	Collection() *mgm.Collection
+}
+
 // Connect provides abstraction to get access to MongoDB
 func Connect() error {
 	return mgm.SetDefaultConfig(nil, os.Getenv("DBNAME"), options.Client().ApplyURI(os.Getenv("DBURI")))

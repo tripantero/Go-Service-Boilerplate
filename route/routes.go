5a0e9144f62fdaec0db3ebea3a9c7d388bb9c551
Entalizen <danielarya185@gmail.com>
2020-08-03 19:49:29 +0900

    feat(route): apply cors on route service

--- route/routes.go
+++ route/routes.go
@@ -2,13 +2,15 @@ package route
 
 import (
 	v1 "github.com/Satssuki/Go-Service-Boilerplate/controllers/api/v1"
+	"github.com/Satssuki/Go-Service-Boilerplate/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 // SetupRouter Register each route here
 func SetupRouter() *gin.Engine {
-	router := gin.Default()
-
+	router := gin.Default()	
+	router.Use(middlewares.AppliAllCORS)
+	
 	v1g := router.Group("/api/v1")
 	{
 		v1g.POST("/user", v1.InsertUser)

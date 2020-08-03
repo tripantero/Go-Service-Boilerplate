71d5b6defe4b97135e6cdaf97afe009c3a9b6cde
Entalizen <danielarya185@gmail.com>
2020-08-03 19:24:41 +0900

    refactor(services/api/v1): i am forget to return error when insert document in user struct

--- services/api/v1/user.go
+++ services/api/v1/user.go
@@ -20,8 +20,7 @@ func CreateUserService() UserService {
 func (user *UserService) Insert() error {
 	if len(user.User.Name) >= 4 && user.User.Age > 0 {
 		currentUser := &user.User
-		currentUser.GetCollection().Create(currentUser)
-		return nil
+		return currentUser.GetCollection().Create(currentUser)
 	}
 	return errors.New("Model not valid")
 }

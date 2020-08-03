50e79fbedbbabb2ed9fe2710bef3bea07d2d3633
Entalizen <danielarya185@gmail.com>
2020-08-03 18:44:41 +0900

    feat(models): implement BaseModel action to struct user

--- models/user.go
+++ models/user.go
@@ -10,3 +10,8 @@ type User struct {
 	Name             string `bson:"name" json:"name"`
 	Age              int    `bson:"age"  json: "age"`
 }
+
+// Collection return the collection of current user model
+func (user *User) Collection() *mgm.Collection {
+	return mgm.Coll(user)
+}

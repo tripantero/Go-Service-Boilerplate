2d6e590f7f844f1850a4978144f6e9b40aa7648e
Entalizen <danielarya185@gmail.com>
2020-08-04 21:31:03 +0900

    perf(services/api/v1/): prevent multiple user registered in database collection

--- services/api/v1/user.go
+++ services/api/v1/user.go
@@ -1,8 +1,12 @@
 package v1
 
 import (
+	"errors"
+
+	"github.com/Kamva/mgm/v3"
 	"github.com/Satssuki/Go-Service-Boilerplate/models"
 	"github.com/Satssuki/Go-Service-Boilerplate/services/api/validation"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // UserService struct that wrapper the user model api
@@ -20,7 +24,16 @@ func (user *UserService) Insert() error {
 	err := validation.ValidateUser(&user.User)
 	if err == nil {
 		currentUser := &user.User
-		return currentUser.GetCollection().Create(currentUser)
+		count, err := currentUser.GetCollection().CountDocuments(mgm.Ctx(), bson.M{
+			"name": user.User.Name,
+			"age":  user.User.Age,
+		})
+		if count > 0 {
+			return errors.New("User already there")
+		} else {
+			err = currentUser.GetCollection().Create(currentUser)
+		}
+		return err
 	}
 	return err
 }
